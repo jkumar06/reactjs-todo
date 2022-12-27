3c32375bf6eb3da8566c4c01f4f9153def2b014c
jagadeesh kumar <jagadeesh.kumar@bolt.earth>
2022-12-27 21:05:41 +0530

    todo list app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+  const [todo,setTodo] = useState("");
+  const [todos,setTodos] = useState([]);
+  const [editId,setEditId] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(editId) {
+      const editTodo = todos.find((i) => i.id === editId);
+      const updatedTodos = todos.map((t) =>
+       t.id === editTodo.id
+       ?(t = {id:t.id,todo}) : {id:t.id,todo:t.todo}
+      )
+        setTodos(updatedTodos);
+        setEditId(0);
+        setTodo("")
+        return;
+    }
+
+    if(todo !== "") {
+      setTodos([{id:`${todo}-${Date.now()}`,todo},...todos]);
+      setTodo("")
+    }
+  }
+
+  const deleteHandler = (id) => {
+      const deleteTodo = todos.filter((to) => to.id !== id);
+      setTodos([...deleteTodo])
+  }
+
+  const editHandler = (id) => {
+      const editTodo = todos.find((i) =>i.id === id);
+      setTodo(editTodo.todo);
+      setEditId(id);
+  }
+
+  return(
+    <>
+    <div className='App'>
+        <div className='container'>
+          <h2>Todo List App</h2>
+          <form className='todoForm' onSubmit={handleSubmit}>
+            <input type= "text" value={todo} onChange={(e)=>setTodo(e.target.value)} />
+            <button type='submit'> {editId ? "Edit": "Go"}</button>
+          </form>
+          <ul className='allTodos'>
+            {todos.map((t) => 
+              <li className='singleTodo'>
+                <span className='todoText' key={t.id}>{t.todo}</span>
+            <button onClick={() => editHandler(t.id)}>Edit</button>
+            <button onClick={() => deleteHandler(t.id)}>Delete</button></li> 
+             )}
+          </ul>
+        </div>
     </div>
-  );
+      
+      
+    </>
+  ) 
 }
 
 export default App;
